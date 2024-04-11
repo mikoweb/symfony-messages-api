07be9e39014c23b25319c064c831d415c1ce402c
Rafał Mikołajun <root@rmweb.pl>
2024-04-11 22:37:45 +0200

    initial commit

---
+++ src/Core/UI/Dto/Api/Response/ErrorResponseDto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Core\UI\Dto\Api\Response;
+
+class ErrorResponseDto implements ErrorResponseInterface
+{
+    public function __construct(
+        private string $error
+    ) {}
+
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    public function setError(string $error): static
+    {
+        $this->error = $error;
+
+        return $this;
+    }
+}
