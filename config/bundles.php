c130b5392d5609e790b6c7cc9e73c81bab63c858
Rafał Mikołajun <root@rmweb.pl>
2024-04-14 19:29:44 +0200

    Doctrine added

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
