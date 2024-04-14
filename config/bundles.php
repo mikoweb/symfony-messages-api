b034fa92dbc0f23ddf55ae40ff4c83f2d666c244
Rafał Mikołajun <root@rmweb.pl>
2024-04-14 20:50:35 +0200

    Doctrine MongoDB added

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
